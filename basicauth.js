049b552dfb0ea1e825824a063c0afa4f84402aa3
rajrin@yahoo.com <raj@acloudfan.com>
2016-11-11 08:20:44 -0500

    minor tweaks

--- basicauth.js
+++ basicauth.js
@@ -3,8 +3,10 @@ var passport = require('passport')
 // This the strategy for basic authentication
 var BasicStrategy = require('passport-http').BasicStrategy
 
+// Access to the users data
 var users = require(__dirname + '/userdata/users')
 
+// Setup the passport strategy
 passport.use(new BasicStrategy(function (username, password, done) {
 
     var user = users.checkCredentials(username,password)
@@ -14,6 +16,7 @@ passport.use(new BasicStrategy(function (username, password, done) {
         return done(null, false)
 }));
 
+// This is the middleware function that gets invoked
 var auth = passport.authenticate('basic', { session: false })
 
 exports.auth = auth;
--- userdata/users.js
+++ userdata/users.js
@@ -1,12 +1,13 @@
+// Hardcoded users for testing
+// Can be changed to store the users in a database
 var users = [
-    { id: 1, name: "john", email: "john@mail.com", password: "john123" },
-    { id: 2, name: "sarah", email: "sarah@mail.com", password: "sarah123" }
+    { id: 1, name: "jim", email: "jim@mail.com", password: "jim123" },
+    { id: 2, name: "sam", email: "sam@mail.com", password: "sam123" }
 ];
 
 
 var checkCredentials = function (username, password) {
     // Check if username/password are good
-
     var user = users.find(function (u) {
         return u.name === username && u.password === password;
     });
