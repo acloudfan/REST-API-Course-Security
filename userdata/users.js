31d8dfa58d3c908983cc8dd2726bced6531924db
R S <rajeev@Rajeevs-MBP.hsd1.nj.comcast.net>
2016-11-10 22:26:59 -0500

    ss

---
+++ userdata/users.js
@@ -0,0 +1,6 @@
+var users = [
+    {id: 1, name: "John", email: "john@mail.com", password: "john123"},
+    {id: 2, name: "Sarah", email: "sarah@mail.com", password: "sarah123"}
+];
+
+module.exports = users;
