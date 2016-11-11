331c13371a977db7a972a41bdb9cc1104fe64be6
R S <rajeev@Rajeevs-MBP.hsd1.nj.comcast.net>
2016-11-10 22:11:44 -0500

    jwt cchanges

---
+++ tokens/jwtauth.js
@@ -0,0 +1,29 @@
+// Passport setup for tokens
+var passport = require('passport');
+var passportJWT = require("passport-jwt");
+var extractJwt = passportJWT.ExtractJwt;
+var JwtStrategy = passportJWT.Strategy;
+
+var users = require(__dirname + '/tokens/users')
+
+// Set the jwt location in the request
+// Can be header, query param, body 
+// Set it to header
+var params = {
+    secretOrKey: 'MyS3cr3tK3Y',
+    jwtFromRequest: extractJwt.fromAuthHeader()
+};
+
+// Create the Passport Jwt Startegy
+var strategy = new JwtStrategy(params, function (payload, done) {
+    var user = users[payload.id] || null;
+    if (user) {
+        return done(null, { id: user.id });
+    } else {
+        return done(new Error("User not found"), null);
+    }
+});
+
+// passport.initialize();
+
+exports.auth = passport.authenticate('jwt', { session: false });
