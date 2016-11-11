cda99867d31c75ffe2c7ce77a823384c6e139d20
acloudfan <rajeevsakhuja@40HW015529.hsd1.nj.comcast.net>
2016-11-11 18:00:42 -0500

    upd

---
+++ tokens/tokenstore.js
@@ -0,0 +1,24 @@
+// Validates the token passed by the caller
+// REMOVE
+// https://www.sitepoint.com/using-json-web-tokens-node-js/
+
+// {tone:tok, expires:timestamp}
+var  tokens = [];
+
+module.exports = {
+    add: function(token, payload) {
+        tokens[token] = payload
+    },
+
+    isValid: function(token){
+        if (tokens[token] === undefined) return false;
+
+        if(tokens[token].exp <= new Date()){
+            var ndx = tokens.indexOf(token)
+            tokens.splice(ndx,1)
+            return false
+        } else {
+            return true
+        }
+    }
+}
---
+++ tokens/validator.js
@@ -0,0 +1,42 @@
+// Stores the issued tokens in memory
+// DO NOT USE THIS IN PRODUCTION Implementation
+// This is just for demo purposes
+
+var jwt = require("jwt-simple");
+var jwtAuth = require(__dirname + '/jwtauth')
+var tokenStore = require(__dirname + '/tokenstore')
+
+var  auth = function(req, res, next) {
+    
+    // Token sent by the client HTTP header X-ACME-Token
+    var token = req.headers[jwtAuth.params.ACME_TOKEN_HEADER];
+    //console.log(token)
+    if(token === undefined){
+        
+        // Send 401 with reason for failure
+        res.statusMessage = 'Unauthorized : Token not provided!!!'
+        res.sendStatus('401').end()
+        // Do NOT call next()
+
+    } else {
+        // Decode the header
+        try{
+            var decoded = jwt.decode(token, jwtAuth.params.JWT_TOKEN_SECRET)
+        }catch(e) {
+            // Decode exception
+            res.statusMessage = 'Unauthorized : Invalid Token!!!'
+            res.sendStatus('401')
+            return;
+        }
+
+        // Token is valid so check if it's expired
+        if(!tokenStore.isValid(token)){
+            res.statusMessage = 'Unauthorized : Token invalid or Expired!!!'
+            res.sendStatus('401')
+            return;
+        }
+        next();
+    }
+}
+
+exports.auth = auth
