5350558ff8f493a6272c9b0088e467dd07a601b3
R S <rajeev@Rajeevs-MBP.hsd1.nj.comcast.net>
2016-11-10 22:44:40 -0500

    support for userdata

--- basicauth.js
+++ basicauth.js
@@ -1,16 +1,17 @@
 // This has all the code for implementing basic auth
-var     passport= require('passport')
+var passport = require('passport')
 // This the strategy for basic authentication
-var     BasicStrategy = require('passport-http').BasicStrategy
+var BasicStrategy = require('passport-http').BasicStrategy
 
-passport.use(new BasicStrategy(function(username,password,done){
-    // This is the function that gets called for all protected resource
-    if(username === 'user' && password === 'pass')
-        done(null, true)
-    else   
-        done(null,false)
+var users = require(__dirname + '/userdata/users')
+
+passport.use(new BasicStrategy(function (username, password, done) {
+
+    var check = users.checkCredentials(username,password)
+    return done(null, check)
+
 }));
 
-var auth  = passport.authenticate('basic',{session:false})
+var auth = passport.authenticate('basic', { session: false })
 
 exports.auth = auth;
